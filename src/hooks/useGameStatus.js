68738de30a8f900745bc119208621841b421ae05
suvhan <suvhan13@gmail.com>
2025-06-15 17:01:48 +0530

    first commit

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,24 +1,21 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from "react";
 
-export const useGameStatus = rowsCleared => {
+export const useGameStatus = (rowsCleared) => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200];
-
   const calcScore = useCallback(() => {
-    // We have score
+    const linePoints = [40, 100, 300, 1200];
     if (rowsCleared > 0) {
-      // This is how original Tetris score is calculated
-      setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
-      setRows(prev => prev + rowsCleared);
+      setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
+      setRows((prev) => prev + rowsCleared);
     }
-  }, [level, linePoints, rowsCleared]);
+  }, [level, rowsCleared]);
 
   useEffect(() => {
     calcScore();
-  }, [calcScore, rowsCleared, score]);
+  }, [calcScore]);
 
   return [score, setScore, rows, setRows, level, setLevel];
 };
